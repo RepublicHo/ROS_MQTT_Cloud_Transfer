fa9b1ae548daa95f70c8d0156966bfd3f18a6d9a
Che-tsun Ho <republicho@foxmail.com>
2023-06-29 17:02:32 +0800

    update

--- device/status_notifier.py
+++ device/status_notifier.py
@@ -1,75 +1,169 @@
 import paho.mqtt.client as mqtt
+import threading
+import atexit
 import time
 
-# please make it like a daemon process
-# Define MQTT client callback functions
-def on_connect(client, userdata, flags, rc):
-    print("Connected to MQTT broker with result code "+str(rc))
-    # Publish initial status update
-    client.subscribe("iot_device/command")
-    client.loop_start()
-    for i in range(5):
-        # Publish a message to the "example/topic" topic every 5 seconds
-        client.publish("iot_device/status", "connected")
-        print("daemon process publishes to iot_device/status indicating it's there")
-        time.sleep(1)
-    
 
-def on_message(client, userdata, msg):
-    # print("Received message: "+str(msg.payload.decode()))
-    # If command is "status_check", respond with current status
-    # if msg.payload.decode() == "status_check":
-    print("iot device obtains the message in iot_device/command")
-    # Subscribe to command topic
-    for i in range(5):
-        send_status_updates(client)
-        time.sleep(1)
-
-def connect(self):
-    """
-    Connect to the MQTT broker
-    """
-    while self.rc != 0:
-        try:
-            self.rc = self.client.connect()
-        except:
-            # If the connection fails, wait for 2 seconds before trying again
-            print("Connection failed")
-            print("Kindly note that please have mosquitto properly installed and configured. \n From Mosquitto 2 and higher its only possible to login with localhost on that device. "
-                    +"\nFor another device you have to change some settings in mosquitto.conf. ")
-        time.sleep(2)
-        self.timeout += 2
-
-def on_disconnect(self, client, userdata, rc):
-    """
-    Callback function called when the client is unexpectedly disconnected from the broker
-    """
-    if rc != 0:
-        if not self.disconnect_flag:
-            print("Unexpected disconnection.")
-            print("Trying reconnection")
-            self.rc = rc
-            self.connect() 
-
-def send_status_updates(client):
-    # Send a series of status updates every second for 10 seconds
-    
-    client.publish("iot_device/command_response", "status_ok")
-    print("sent to iot_device/command_response")
-    time.sleep(1)
+class StatusNotifierBridge:
+    def __init__(self, command_topic = "iot_device/command", 
+                 client_id="device", user_id="", password="", 
+                 host="43.133.159.102", port=1883, keepalive=60, qos=0):
+       
+        self.command_topic = command_topic
+        self.client_id = client_id
+        self.user_id = user_id
+        self.password = password
+        self.host = host
+        self.port = port
+        self.keepalive = keepalive
+        self.qos = qos
         
+
+        self.disconnect_flag = False
+        self.rc = 1
+        self.timeout = 0
+
+        # Create the MQTT client object, set the username and password, and register the callback functions
+        self.client = mqtt.Client(self.client_id, clean_session=True)
+        self.client.username_pw_set(self.user_id, self.password)
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_message = self.on_message
+        self.client.on_disconnect = self.on_disconnect
+
+        # Connect to the broker
+        self.connect()
+
+    def connect(self):
+        """
+        Connect to the MQTT broker
+        """
+        while self.rc != 0:
+            try:
+                self.rc = self.client.connect(self.host, self.port, self.keepalive)
+            except:
+                # If the connection fails, wait for 2 seconds before trying again
+                print("Connection failed")
+                
+            time.sleep(2)
+            self.timeout += 2
+
+    def disconnect(self):
+        """
+        Disconnect from the MQTT broker
+        """
+        print("Disconnecting")
+        self.disconnect_flag = True
+        self.client.disconnect()
+        
+    def msg_process(self, msg):
+        """
+        Process incoming MQTT messages
+        """
+        print("processing message")
+        msg = str(msg.payload.decode())
+        if msg == "status_check":
+            self.publish("iot_device/command_response", "status_ok")
+            print("sent to iot_device/command_response")
+        pass
+
+    def looping(self, loop_timeout=30):
+        """
+        Start the MQTT client's event loop
+        :param loop_timeout: The maximum time to wait for incoming messages before returning
+        """
+        # self.client.loop(loop_timeout)
+        self.client.loop_forever()
+
+    def on_connect(self, client, userdata, flags, rc):
+        """
+        Callback function called when the client successfully connects to the broker
+        """
+        print(f"Connected to MQTT broker with result code {str(rc)}")
+        self.client.subscribe(self.command_topic)
+        self.timeout = 0
+        
+        # start a thread to publish status
+        status_thread = threading.Thread(target=self.publish_status)
+        status_thread.start()
+        atexit.register(self.stop_publish_status_thread, status_thread)
+    
+    def publish_status(self):
+        """
+        Continuously publish device status every second
+        """
+        for i in range(20):
+            self.publish("iot_device/status", "connected")
+            print("daemon process publishes to iot_device/status indicating it's there")
+            time.sleep(3)
+            
+    def stop_publish_status_thread(self, thread):
+        print("Stopping thread")
+        thread.join()
+        
+    def on_disconnect(self, client, userdata, rc):
+        """
+        Callback function called when the client is unexpectedly disconnected from the broker
+        """
+        if rc != 0:
+            if not self.disconnect_flag:
+                print("Unexpected disconnection.")
+                print("Trying reconnection")
+                self.rc = rc
+                self.connect()
+
+    def on_message(self, client, userdata, msg):
+        """
+        Callback function called when an incoming message is received
+        """
+        print("iot device obtains the message in iot_device/command")
+        self.msg_process(msg)
+
+    def unsubscribe(self):
+        """
+        Unsubscribe from the MQTT topic
+        """
+        print("Unsubscribing")
+        self.client.unsubscribe(self.command_topic)
+
+    def disconnect(self):
+        """
+        Disconnect from the MQTT broker
+        """
+        print("Disconnecting")
+        self.disconnect_flag = True
+        self.client.disconnect()
+
+    
+    def publish(self, topic, message):
+        """
+        Publish a message to the MQTT broker
+        :param message: The message to publish
+        """
+        self.client.publish(topic, message)
+
+    def hook(self):
+        """
+        Gracefully shut down the MQTT client
+        """
+        self.unsubscribe()
+        self.disconnect()
+        print("Shutting down")
+
+    def get_timeout(self):
+        """
+        Get the amount of time elapsed since the connection attempt started
+        """
+        return self.timeout
+
 if __name__ == "__main__":
     
     # Set up MQTT client and callbacks
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.on_disconnect = on_disconnect
-    # Connect to MQTT broker and start client loop
-    client.connect()
+    sn = StatusNotifierBridge()
+    sn.looping()
     
     # Continuously publish device status every 5 seconds
 
     # Stop MQTT client loop and disconnect from broker
-    client.loop_forever()
+    
 
--- host/iot_status_checker.py
+++ host/iot_status_checker.py
@@ -15,11 +15,12 @@ class StatusChecker:
 
     def on_connect(self, client, userdata, flags, rc):
         print(f"Connected to MQTT broker with result code {rc}")
+        
         if rc == 0:
             self.connected = True
             # Subscribe to the device's status topic    
             client.subscribe("iot_device/status")
-            
+            print(1)
         else:
             self.connected = False
 
@@ -30,11 +31,15 @@ class StatusChecker:
             self.dict['status_received'] = True
             print("status received here")
             client.publish("iot_device/command", "status_check")
+            client.unsubscribe("iot_device/status")
             client.subscribe("iot_device/command_response")
+            print("unsubscribe ot_device/command")
         elif msg.topic == "iot_device/command_response":
             # Set flag indicating that the device is responding to commands
             self.dict['command_response_received'] = True
             print("!!!great, response received here")
+            client.unsubscribe("iot_device/command_response")
+            print("unsubscribe response")
 
     def connect(self):
         # Connect to MQTT broker and start client loop
@@ -46,17 +51,17 @@ class StatusChecker:
             print("trying to reconnect to the broker :(")
             time.sleep(1)
 
-    def disconnect(self):
-        # Stop client loop and disconnect from MQTT broker
-        self.client.loop_stop()
-        self.client.disconnect()
+    # def disconnect(self):
+    #     # Stop client loop and disconnect from MQTT broker
+    #     self.client.loop_stop()
+    #     self.client.disconnect()
 
     def send_command(self):
         # Send command to device to verify connectivity
-        for i in range(10):
-            print("Sending command to iot_device/command")
-            self.client.publish("iot_device/command", "status_check")
-            time.sleep(1)
+        
+        print("Sending command to iot_device/command")
+        self.client.publish("iot_device/command", "status_check")
+        time.sleep(1)
 
     def check_device_status(self, timeout=20.0):
         # Wait for timeout seconds or until a status update and command response are received
@@ -90,13 +95,28 @@ class StatusChecker:
             verification_status = self.check_device_status(timeout)
 
             # Disconnect from MQTT broker and return device verification status
-            self.disconnect()
+            # self.disconnect()
+            self.client.loop_stop()
             return verification_status
         else:
+            self.client.loop_stop()
             return False
 
 
 # Test code
 if __name__ == '__main__':
-    cli = StatusChecker()
-    print(cli.get_device_status())
+    max_attempts = 3
+    delay = 1
+    attempts = 0
+    while attempts < max_attempts:
+        try:
+            cli = StatusChecker()
+            print(cli.get_device_status())
+            exit()
+        except Exception as e:
+            print() 
+            print(f"Exception occurred ({str(e)}), retrying in {delay} seconds")
+            time.sleep(delay)
+            attempts += 1
+            
+    print(f"number of attempts is ({attempts})")
